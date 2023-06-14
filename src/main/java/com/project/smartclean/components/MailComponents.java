b6bdd394f3b525be01becbb1b25264afa6f264e9
chenj <chenj7498@gmail.com>
2023-06-14 11:49:20 +0900

    회원 관련 인증 메일 설정

---
+++ src/main/java/com/project/smartclean/components/MailComponents.java
@@ -0,0 +1,40 @@
+package com.project.smartclean.components;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
+
+@Component
+@RequiredArgsConstructor
+public class MailComponents {
+    private final JavaMailSender javaMailSender;
+
+    public boolean sendMail(String mail, String subject, String text) {
+
+        boolean result = false;
+
+        MimeMessagePreparator msg = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                mimeMessageHelper.setFrom("yum_aily@naver.com");
+                mimeMessageHelper.setTo(mail);
+                mimeMessageHelper.setSubject(subject);
+                mimeMessageHelper.setText(text);
+            }
+        };
+
+        try {
+            javaMailSender.send(msg);
+            result = true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+}
