47ac0642f4fa26f4830e395cbe3c89ac73af3ad2
chenj <chenj7498@gmail.com>
2023-06-14 11:53:42 +0900

    주문 및 수거 코드

---
+++ src/main/java/com/project/smartclean/admin/model/PickupCode.java
@@ -0,0 +1,10 @@
+package com.project.smartclean.admin.model;
+
+public interface PickupCode {
+    String  PICKUP_STATUS_WAITING = "WAITING";
+    String PICKUP_STATUS_COMPLETE ="COMPLETE";
+    String PICKUP_STATUS_REJECT="REJECT";
+
+    String ORDER_COMPLETE = "COMPLETE";
+    String ORDER_CANCEL="CANCEL";
+}
